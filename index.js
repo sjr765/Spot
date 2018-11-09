9685c7ce3fe4dff06a84c49389e0bace8994be35
Sean Ryan <sjr765@gmail.com>
2018-11-09 13:24:14 -0500

    testing webhook

--- index.js
+++ index.js
@@ -13,34 +13,35 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'))
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
+  // Parse the request body from the POST
   let body = req.body
-  // Get the sender PSID
-  let sender_psid = webhook_event.sender.id
-  console.log('Sender PSID: ' + sender_psid)
 
-  // Checks this is an event from a page subscription
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
       // Gets the body of the webhook event
-
       let webhook_event = entry.messaging[0]
       console.log(webhook_event)
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id
+      console.log('Sender PSID: ' + sender_psid)
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message)
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback)
+      }
     })
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED')
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404)
-
-    // Check if the event is a message or postback and
-    // pass the event to the appropriate handler function
-    if (webhook_event.message) {
-      handleMessage(sender_psid, webhook_event.message)
-    } else if (webhook_event.postback) {
-      handlePostback(sender_psid, webhook_event.postback)
-    }
   }
 })
 
