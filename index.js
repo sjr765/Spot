b051a1bf734c2c8b30df9194a272fec7c6e4b281
Sean Ryan <sjr765@gmail.com>
2018-11-09 00:05:09 -0500

    Getting users PSID

--- index.js
+++ index.js
@@ -1,55 +1,71 @@
-'use strict';
+'use strict'
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 
 // Imports dependencies and set up http server
 const express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  app = express().use(bodyParser.json()) // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'))
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
-  let body = req.body;
+  let body = req.body
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
+      // Gets the message. entry.messaging is an array, but will only ever contain one message, so we get index 0
+
+      // Gets the body of the webhook event
+
+      let webhook_event = entry.messaging[0]
+      console.log(webhook_event)
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id
+      console.log('Sender PSID: ' + sender_psid)
+    })
 
     // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
+    res.status(200).send('EVENT_RECEIVED')
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
+    res.sendStatus(404)
   }
-});
+})
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = 'SPOT_THE_CHAT_BOT';
+  let VERIFY_TOKEN = 'SPOT_THE_CHAT_BOT'
 
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  let mode = req.query['hub.mode']
+  let token = req.query['hub.verify_token']
+  let challenge = req.query['hub.challenge']
 
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+      console.log('WEBHOOK_VERIFIED')
+      res.status(200).send(challenge)
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+      res.sendStatus(403)
     }
   }
-});
+})
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
