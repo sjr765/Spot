5b810b6a0b2e7d47327659043eb10866cda6a42e
Sean Ryan <sjr765@gmail.com>
2018-11-10 23:10:08 -0500

    repeat test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const {toneAnalyzer} = require('./toneAnalyzer')
+const {toneAnalyzer} = require('./toneAnalyzer')
 
 const request = require('request')
 const path = require('path')
