79afbc0c5cfb6826e2707cabbf95dc8af6a15a35
Sean Ryan <sjr765@gmail.com>
2018-11-12 00:44:00 -0500

    test

--- index.js
+++ index.js
@@ -200,32 +200,6 @@ function handleMessage(sender_psid, received_message) {
         }
       })
 
-      try {
-        axios({
-          method: 'get',
-          url: 'https://api.spotify.com/v1/recommendations',
-          headers: {
-            Authorization: 'Bearer ' + spotifyUserToken
-          },
-          params: {
-            limit: '1',
-            market: 'US',
-            seed_genres: genres.joy,
-            min_popularity: '20'
-          }
-        }).then(response => {
-          console.log('RESPONSE.DATA.TRACKS', response.data.tracks[0])
-          console.log(
-            '*****WHOLE RESPONSE*****',
-            response.data.tracks[0].external_urls
-          )
-          recommendedSong = response.data.tracks[0].external_urls.spotify
-          res.json(recommendedSong)
-        })
-      } catch (err) {
-        console.error(err)
-      }
-
       // console.log('!!!!!!!!!!!!!!! SPOTIFY API END HERE!!!!!!!!!')
       response = {
         text: 'WOOF! I hope you like this: ' + recommendedSong
@@ -283,4 +257,35 @@ function callSendAPI(sender_psid, response) {
       }
     }
   )
+}
+
+async function getSpotifyData() {
+  try {
+    const url = await axios({
+      method: 'get',
+      url: 'https://api.spotify.com/v1/recommendations',
+      headers: {
+        Authorization: 'Bearer ' + spotifyUserToken
+      },
+      params: {
+        limit: '1',
+        market: 'US',
+        seed_genres: genres.joy,
+        min_popularity: '20'
+      }
+    })
+    console.log('=========URL =======', url)
+    return url
+    // .then(response => {
+    //   console.log('RESPONSE.DATA.TRACKS', response.data.tracks[0])
+    //   console.log(
+    //     '*****WHOLE RESPONSE*****',
+    //     response.data.tracks[0].external_urls
+    //   )
+    //   recommendedSong = response.data.tracks[0].external_urls.spotify
+    //   res.json(recommendedSong)
+    // })
+  } catch (err) {
+    console.error(err)
+  }
 }
