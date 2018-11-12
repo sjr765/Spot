d716c79b493d1e02433d47c33b457fa6c999ec71
Sean Ryan <sjr765@gmail.com>
2018-11-11 23:24:44 -0500

    test

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ function handleMessage(sender_psid, received_message) {
           console.log('======== TONE ANALYSIS FROM WATSON ============')
           console.log(JSON.stringify(toneAnalysis, null, 1))
           let tone = JSON.stringify(toneAnalysis.document_tone.tones[0].tone_id)
-          finalGenre = genres.tone
+          finalGenre = genres[tone]
           console.log('FINAL GENRE =======', finalGenre)
 
           console.log('======== END OF TONE ANALYSIS FROM WATSON ============')
