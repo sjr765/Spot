1e251a36bf38438cd7fc419e1120781df452f383
Sean Ryan <sjr765@gmail.com>
2018-11-12 11:15:04 -0500

    test

--- index.js
+++ index.js
@@ -248,61 +248,63 @@ function handleMessage(sender_psid, received_message) {
       })
 
       // console.log('!!!!!!!!!!!!!!! SPOTIFY API END HERE!!!!!!!!!')
-  //     console.log('======finalSong OUTSIDE of Spotify call=======', finalSong)
-  //     response = {
-  //       text: 'WOOF, I hope you enjoy this: ' + finalSong
-  //     }
-  //     counter = -1
-  //   }
-  // }
-  // console.log('====== COUNTER IS: ', counter)
-  // console.log('====== CONCATENATED REPONSE IS: ', userResponse)
-  counter++
-  // Sends the response message
-  callSendAPI(sender_psid, response)
-}
+      //     console.log('======finalSong OUTSIDE of Spotify call=======', finalSong)
+      //     response = {
+      //       text: 'WOOF, I hope you enjoy this: ' + finalSong
+      //     }
+      //     counter = -1
+      //   }
+      // }
+      // console.log('====== COUNTER IS: ', counter)
+      // console.log('====== CONCATENATED REPONSE IS: ', userResponse)
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-  let response
-
-  // Get the payload for the postback
-  let payload = received_postback.payload
-
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = {text: 'Thanks!'}
-  } else if (payload === 'no') {
-    response = {text: 'Oops, try sending another image.'}
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response)
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    recipient: {
-      id: sender_psid
-    },
-    message: response
-  }
-  // Send the HTTP request to the Messenger Platform
-  request(
-    {
-      uri: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: PAGE_ACCESS_TOKEN},
-      method: 'POST',
-      json: request_body,
-      messaging_type: 'RESPONSE'
-    },
-    (err, res, body) => {
-      if (!err) {
-        console.log('message sent!')
-      } else {
-        console.error('Unable to send message:' + err)
-      }
+      counter++
+      // Sends the response message
+      callSendAPI(sender_psid, response)
     }
-  )
+  }
+  // Handles messaging_postbacks events
+  function handlePostback(sender_psid, received_postback) {
+    let response
+
+    // Get the payload for the postback
+    let payload = received_postback.payload
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = {text: 'Thanks!'}
+    } else if (payload === 'no') {
+      response = {text: 'Oops, try sending another image.'}
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response)
+  }
+
+  // Sends response messages via the Send API
+  function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      recipient: {
+        id: sender_psid
+      },
+      message: response
+    }
+    // Send the HTTP request to the Messenger Platform
+    request(
+      {
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: request_body,
+        messaging_type: 'RESPONSE'
+      },
+      (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error('Unable to send message:' + err)
+        }
+      }
+    )
+  }
 }
